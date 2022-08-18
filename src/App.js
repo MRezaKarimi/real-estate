19d5665f49fc18b7508fa0d5d48ff5a6aeba5108
mark <mr.karimi1377@gmail.com>
2022-08-18 21:05:19 +0430

    add tailwind, postcss and autoprefixer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow-gray-300 shadow-sm border p-8 m-10">
+        <p className="text-3xl text-gray-900 font-bold mb-5">Welcome!</p>
+        <p className="text-gray-500 text-lg">
+          React and Tailwind CSS in action
         </p>
       </div>
     );
