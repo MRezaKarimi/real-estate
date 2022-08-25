20af42fe88639112b1e22ce83f2ef499e1ad3ca3
mark <mr.karimi1377@gmail.com>
2022-08-25 20:44:22 +0430

    add property item component

---
+++ src/components/propertyItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ThumbSlider from "./thumbSlider";
+
+const PropertyItem = ({ property }) => {
+  return (
+    <div className="flex flex-col">
+      <ThumbSlider images={property.images} />
+      <div className="flex flex-row justify-between pt-1">
+        <p className="self-center text-xl font-medium">${property.price}</p>
+        <p className="self-center text-md font-medium text-gray-800">
+          {property.bed} Bed. {property.bath} Bath.
+        </p>
+      </div>
+      <p className="text-gray-600">{property.address}</p>
+    </div>
+  );
+};
+
+export default PropertyItem;
