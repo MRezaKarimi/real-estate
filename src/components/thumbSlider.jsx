ffb4a54f963f06c8a219be386b781b4ae5e41fcb
mark <mr.karimi1377@gmail.com>
2022-08-25 20:43:28 +0430

    add thumb slider component

---
+++ src/components/thumbSlider.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+
+const ThumbSlider = ({ images }) => {
+  const [index, setIndex] = useState(0);
+
+  const nextImage = () => {
+    if (index === images.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  const previousImage = () => {
+    if (index === 0) {
+      setIndex(images.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+
+  console.log(images[index]);
+
+  return (
+    <React.Fragment>
+      <div
+        style={{ backgroundImage: `url(${images[index]})` }}
+        className={`flex flex-col h-48 rounded-lg bg-cover bg-no-repeat`}
+      >
+        <div className="flex flex-row items-center grow">
+          <ChevronLeftIcon
+            className="w-7 h-7 p-1 bg-white opacity-75 rounded-full mx-1"
+            onClick={previousImage}
+          />
+          <div className="grow"></div>
+          <ChevronRightIcon
+            className="w-7 h-7 p-1 bg-white opacity-75 rounded-full mx-1"
+            onClick={nextImage}
+          />
+        </div>
+      </div>
+
+      {/* <div className="flex flex-row justify-center">.....</div> */}
+    </React.Fragment>
+  );
+};
+
+export default ThumbSlider;
