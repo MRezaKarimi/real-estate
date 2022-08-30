95e3318c2bbce6f387fb61e6b93ce8fcd2ca37bb
mark <mr.karimi1377@gmail.com>
2022-08-30 21:05:13 +0430

    load propert list from json

--- src/components/propertyList.jsx
+++ src/components/propertyList.jsx
@@ -1,18 +1,8 @@
 import React from "react";
 import PropertyItem from "./propertyItem";
+import propertyList from "../services/fakeData.json";
 
 const PropertyList = () => {
-  const propertyList = Array(10).fill({
-    bed: 2,
-    bath: 3,
-    price: 2500000,
-    address: "5th Ave, Manhattan",
-    images: [
-      "https://img.jamesedition.com/listing_images/2022/05/20/15/54/03/e6000997-394f-4c6e-b0b5-2c51a5c22c76/je/556x342xcxm.jpg",
-      "https://img.jamesedition.com/listing_images/2022/05/20/15/54/03/bb1d8c38-a63a-4196-bda1-0dc6e411c6be/je/556x342xcxm.jpg",
-    ],
-  });
-
   return (
     <div className="basis-1/2">
       <div className="grid grid-cols-2 gap-4 px-4">
