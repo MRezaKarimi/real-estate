b7a43d83e1f60cc9e6dbee60c18a8dd6f4345797
mark <mr.karimi1377@gmail.com>
2022-09-09 20:02:34 +0430

    wrap property detatils with <a> tag

--- src/components/propertyItem.jsx
+++ src/components/propertyItem.jsx
@@ -6,15 +6,17 @@ const PropertyItem = ({ property }) => {
   return (
     <div className="flex flex-col">
       <ImageSlider images={property.images} />
-      <div className="flex flex-row justify-between pt-1">
-        <p className="self-center text-xl font-medium">
-          ${numToMoney(property.price)}
-        </p>
-        <p className="self-center text-md font-medium text-gray-800">
-          {property.bed} Bed. {property.bath} Bath.
-        </p>
-      </div>
-      <p className="text-gray-600">{property.address}</p>
+      <a href={`/property/${property.id}`} target="blank">
+        <div className="flex flex-row justify-between pt-1">
+          <p className="self-center text-xl font-medium">
+            ${numToMoney(property.price)}
+          </p>
+          <p className="self-center text-md font-medium text-gray-800">
+            {property.bed} Bed. {property.bath} Bath.
+          </p>
+        </div>
+        <p className="text-gray-600">{property.address}</p>
+      </a>
     </div>
   );
 };
