cf8ae2ad73bb7ae85689e37391d089eb82c37bb9
mark <mr.karimi1377@gmail.com>
2022-09-11 20:38:47 +0430

    use fontAwesome instead of hero icons. refactor code.

--- src/components/imageSlider.jsx
+++ src/components/imageSlider.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ImageSlider = ({ images }) => {
   const [index, setIndex] = useState(0);
@@ -26,22 +30,21 @@ const ImageSlider = ({ images }) => {
     <React.Fragment>
       <div
         style={{ backgroundImage: `url(${images[index]})` }}
-        className={`flex flex-col h-48 rounded-lg bg-cover bg-no-repeat`}
+        className={`flex flex-col h-48 rounded-xl bg-cover bg-no-repeat`}
       >
-        <div className="flex flex-row items-center grow">
-          <ChevronLeftIcon
-            className="w-7 h-7 p-1 bg-white opacity-75 rounded-full mx-1"
+        <div className="flex flex-row items-center justify-between grow">
+          <FontAwesomeIcon
+            className="w-5 h-5 p-1 bg-white opacity-75 rounded-full mx-1 cursor-pointer"
             onClick={previousImage}
+            icon={faChevronLeft}
           />
-          <div className="grow"></div>
-          <ChevronRightIcon
-            className="w-7 h-7 p-1 bg-white opacity-75 rounded-full mx-1"
+          <FontAwesomeIcon
+            className="w-5 h-5 p-1 bg-white opacity-75 rounded-full mx-1 cursor-pointer"
             onClick={nextImage}
+            icon={faChevronRight}
           />
         </div>
       </div>
-
-      {/* <div className="flex flex-row justify-center">.....</div> */}
     </React.Fragment>
   );
 };
