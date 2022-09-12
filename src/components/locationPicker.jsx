99367b20edc94a118a9d778f932a645aa2f7ae40
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-09-12 14:05:22 +0430

    add locationPicker component

---
+++ src/components/locationPicker.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, useMapEvents, Marker } from "react-leaflet";
+import { markerIcon } from "./markers";
+
+function LocationMarker({ onLocationPicked }) {
+  const [position, setPosition] = useState(null);
+
+  useMapEvents({
+    click: (event) => {
+      if (onLocationPicked) {
+        onLocationPicked(event.latlng.lat, event.latlng.lng);
+      }
+      setPosition(event.latlng);
+    },
+  });
+
+  return position === null ? (
+    <></>
+  ) : (
+    <Marker position={[position.lat, position.lng]} icon={markerIcon}></Marker>
+  );
+}
+
+const LocationPicker = ({ onLocationPicked }) => {
+  return (
+    <MapContainer
+      zoom={11}
+      center={[40.7128, -74.006]}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <TileLayer
+        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <LocationMarker {...{ onLocationPicked }} />
+    </MapContainer>
+  );
+};
+
+export default LocationPicker;
