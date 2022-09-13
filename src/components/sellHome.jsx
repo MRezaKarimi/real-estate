aa58540dfd6f520cc45f5d8bdef0807e99ef1579
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-09-13 13:13:04 +0430

    add property detail form. refactor code.

--- src/components/sellHome.jsx
+++ src/components/sellHome.jsx
@@ -1,60 +1,112 @@
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHome,
+  faLocationDot,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { FilledButton, OutlinedButton } from "./buttons";
 import LocationPicker from "./locationPicker";
+import {
+  emailFieldValidator,
+  phoneFieldValidator,
+  requiredFieldValidator,
+} from "../utils/validators";
+import InputField from "./inputField";
+import SelectorField from "./selectorField";
 
 const SellHome = () => {
+  const steps = [
+    <LocationChooser />,
+    <ContactInfoForm />,
+    <PropertyDetailsForm />,
+  ];
+  const [step, setStep] = useState(0);
+
   return (
     <div className="w-screen sm:w-[500px] m-auto">
-      <div className="text-sky-800 font-bold text-lg sm:text-2xl flex justify-center my-5 items-center">
-        <FontAwesomeIcon icon={faLocationDot} className="mr-3" />
-        <span className="text-center">Choose your location</span>
-      </div>
-      <ContactInfo />
+      {steps[step]}
       <div className="flex justify-between my-5 mx-2 sm:mx-0">
-        <OutlinedButton text="Back" />
-        <FilledButton text="Next" />
+        {step === 0 ? (
+          <div></div>
+        ) : (
+          <OutlinedButton text="Back" onClick={() => setStep(step - 1)} />
+        )}
+        <FilledButton text="Next" onClick={() => setStep(step + 1)} />
       </div>
     </div>
   );
 };
 
-const ChooseLocation = () => {
+const FormHeader = ({ title, icon }) => {
   return (
-    <div className="h-[500px]">
-      <LocationPicker
-        className="mx-2 sm:mx-0"
-        onLocationPicked={(lat, lng) => console.log(lat, lng)}
-      />
+    <div className="text-sky-800 font-bold text-lg sm:text-2xl flex justify-center my-5 items-baseline">
+      <FontAwesomeIcon icon={icon} className="mr-3 text-xl" />
+      <span className="text-center mb-2">{title}</span>
     </div>
   );
 };
 
-const ContactInfo = () => {
+const LocationChooser = () => {
   return (
-    <div>
-      <input
-        type="text"
-        className="bg-gray-100 rounded-lg w-full text-lg p-2 my-2 outline-none focus:border-sky-500 border-2"
+    <>
+      <FormHeader title="Choose your location" icon={faLocationDot} />
+      <div className="h-[500px]">
+        <LocationPicker
+          className="mx-2 sm:mx-0"
+          onLocationPicked={(lat, lng) => console.log(lat, lng)}
+        />
+      </div>
+    </>
+  );
+};
+
+const ContactInfoForm = () => {
+  return (
+    <>
+      <FormHeader title="Add Your Contact Info" icon={faPhone} />
+      <InputField
+        label="First Name"
         placeholder="Your First Name"
-      />
-      <input
         type="text"
-        className="bg-gray-100 rounded-lg w-full text-lg p-2 my-2 outline-none focus:border-sky-500 border-2"
+        validator={requiredFieldValidator}
+      />
+      <InputField
+        label="Last Name"
         placeholder="Your Last Name"
-      />
-      <input
         type="text"
-        className="bg-gray-100 rounded-lg w-full text-lg p-2 my-2 outline-none focus:border-sky-500 border-2"
+        validator={requiredFieldValidator}
+      />
+      <InputField
+        label="Email Address"
         placeholder="Your Email Address"
+        type="email"
+        validator={emailFieldValidator}
       />
-      <input
-        type="text"
-        className="bg-gray-100 rounded-lg w-full text-lg p-2 my-2 outline-none focus:border-sky-500 border-2"
+      <InputField
+        label="Phone Number"
         placeholder="Your Phone Number"
+        type="text"
+        validator={phoneFieldValidator}
       />
-    </div>
+    </>
+  );
+};
+
+const PropertyDetailsForm = () => {
+  return (
+    <>
+      <FormHeader title="Add Your Home Details" icon={faHome} />
+      <div className="flex items-center text-sky-900 font-bold text-2xl">
+        $&nbsp;
+        <InputField label="" />
+        <div className="w-10"></div>m<sup>2</sup>&nbsp;
+        <InputField label="" />
+      </div>
+      <SelectorField label="Bedrooms" />
+      <SelectorField label="Bathrooms" />
+      <InputField label="Address" placeholder="Your Home's Address" />
+    </>
   );
 };
 
