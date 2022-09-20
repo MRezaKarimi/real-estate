db52a06cbe90608bfa8197ca2465d8d327cf4343
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-09-20 14:50:19 +0430

    make navbar responsive. remove backdrop filter. remove home nav link.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,17 +4,19 @@ import NavBarLink from "./navbarLink";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row items-center sticky top-0 px-4 bg-white/80 h-16 z-50 backdrop-blur-md">
-      <Link className="text-sky-900 text-3xl font-semibold mx-5" to="/">
+    <div className="grid grid-cols-4 sm:flex sm:flex-row items-center sticky top-0 px-4 bg-white sm:h-16 z-50 ">
+      <Link
+        className="text-sky-900 text-3xl font-semibold col-span-4 justify-self-center py-2 sm:mx-5"
+        to="/"
+      >
         MyEstate
       </Link>
-      <NavBarLink label="Home" to="/" />
       <NavBarLink label="Buy" to="/buy" />
       <NavBarLink label="Sell" to="/sell" />
-      <div className="grow"></div>
+      <div className="hidden sm:grow sm:block"></div>
       <NavBarLink label="Login" to="/login" />
       <Link
-        className="text-white text-md font-semibold bg-sky-700 rounded-full h-8 mx-3 px-3 py-1"
+        className="text-white text-md font-semibold bg-sky-700 rounded-full h-8 sm:mx-3 px-3 py-1 min-w-max"
         to="/sign-up"
       >
         Sign Up
