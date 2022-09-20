82952f8bbb8f347a1c72b6db5fd49f60de8e4ed4
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-09-20 13:03:02 +0430

    rename home component to main

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./stores/configStore";
 import BuyHome from "./components/buyHome";
-import Home from "./components/home";
+import Main from "./components/main";
 import NavBar from "./components/navbar";
 import "./App.css";
 import SellHome from "./components/sellHome";
@@ -17,7 +17,7 @@ class App extends Component {
           <Routes>
             <Route path="/buy" element={<BuyHome />}></Route>
             <Route path="/sell" element={<SellHome />}></Route>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<Main />}></Route>
           </Routes>
         </BrowserRouter>
       </Provider>
