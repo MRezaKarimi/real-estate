df495db894ff643bbc02a161d9213deb218d4a32
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-09-24 10:46:45 +0330

    use new reducer. rename list to propertyList.

--- src/components/propertyList.jsx
+++ src/components/propertyList.jsx
@@ -3,27 +3,17 @@ import PropertyItem from "./propertyItem";
 import { useDispatch, useSelector } from "react-redux";
 import { Ellipsis } from "react-awesome-spinners";
 import { getProperties } from "../stores/propertySlice";
-import { callApi } from "../actions/api";
 import Pagination from "./pagination";
 
 const PropertyList = () => {
   const [page, setPage] = useState(0);
-  const {
-    list: propertyList,
-    pagesCount,
-    loading,
-  } = useSelector((store) => store.property);
+  const { propertyList, pagesCount, loading } = useSelector(
+    (store) => store.property
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(
-      callApi({
-        url: "/buy",
-        method: "GET",
-        onSuccess: getProperties.type,
-        params: { offset: page, limit: 10 },
-      })
-    );
+    dispatch(getProperties({ page }));
     window.scrollTo(0, 0);
   }, [page]);
 
