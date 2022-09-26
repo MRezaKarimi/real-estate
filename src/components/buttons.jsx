f7cdd2fb4f61ce88de2fff9988618086c1f27cd8
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-09-26 17:08:27 +0330

    change button style

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const OutlinedButton = ({ text, onClick }) => {
   return (
     <div
-      className="border-sky-700 border-2 rounded-lg px-4 py-1 text-sky-700 font-semibold cursor-pointer"
+      className="border-sky-700 h-10 flex items-center justify-center border-2 rounded-lg px-4 py-1 text-sky-700 font-semibold cursor-pointer"
       onClick={onClick}
     >
       {text}
@@ -14,7 +14,7 @@ export const OutlinedButton = ({ text, onClick }) => {
 export const FilledButton = ({ text, onClick }) => {
   return (
     <div
-      className="bg-sky-700 rounded-lg px-4 py-1 text-white font-semibold cursor-pointer"
+      className="bg-sky-700 h-10 flex items-center justify-center rounded-lg px-4 py-1 text-white font-semibold cursor-pointer"
       onClick={onClick}
     >
       {text}
