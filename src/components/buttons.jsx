1de5e4d08c84106f3fee4d9b9ba31bc7dc237080
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-09-29 12:41:30 +0330

    add disable prop

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 
-export const OutlinedButton = ({ text, onClick }) => {
+export const OutlinedButton = ({ text, onClick, disabled = false }) => {
   return (
     <div
-      className="border-sky-700 h-10 flex items-center justify-center border-2 rounded-lg px-4 py-1 text-sky-700 font-semibold cursor-pointer"
-      onClick={onClick}
+      className={`h-10 flex items-center justify-center border-2 rounded-lg px-4 py-1 font-semibold ${
+        disabled
+          ? "cursor-not-allowed border-gray-400 text-gray-400"
+          : "cursor-pointer border-sky-700 text-sky-700"
+      }`}
+      onClick={disabled ? null : onClick}
     >
       {text}
     </div>
   );
 };
 
-export const FilledButton = ({ text, onClick }) => {
+export const FilledButton = ({ text, onClick, disabled = false }) => {
   return (
     <div
-      className="bg-sky-700 h-10 flex items-center justify-center rounded-lg px-4 py-1 text-white font-semibold cursor-pointer"
-      onClick={onClick}
+      className={`h-10 flex items-center justify-center rounded-lg px-4 py-1 text-white font-semibold ${
+        disabled
+          ? "cursor-not-allowed bg-gray-400"
+          : "cursor-pointer bg-sky-700"
+      }`}
+      onClick={disabled ? null : onClick}
     >
       {text}
     </div>
