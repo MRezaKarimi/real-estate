3064e8a259214a661a7027a864b391c9363ec7d1
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-10-01 12:47:12 +0330

    add contactInfoForm component

---
+++ src/components/contactInfoForm.jsx
@@ -0,0 +1,44 @@
+import {
+  emailFieldValidator,
+  phoneFieldValidator,
+  requiredFieldValidator,
+} from "../utils/validators";
+import FormHeader from "./formHeader";
+import InputField from "./inputField";
+
+const ContactInfoForm = ({ style }) => {
+  return (
+    <div
+      style={style}
+      className="transition-opacity ease-in duration-300 overflow-hidden"
+    >
+      <FormHeader title="Add Your Contact Info" />
+      <InputField
+        label="First Name"
+        placeholder="Your First Name"
+        type="text"
+        validator={requiredFieldValidator}
+      />
+      <InputField
+        label="Last Name"
+        placeholder="Your Last Name"
+        type="text"
+        validator={requiredFieldValidator}
+      />
+      <InputField
+        label="Email Address"
+        placeholder="Your Email Address"
+        type="email"
+        validator={emailFieldValidator}
+      />
+      <InputField
+        label="Phone Number"
+        placeholder="Your Phone Number"
+        type="text"
+        validator={phoneFieldValidator}
+      />
+    </div>
+  );
+};
+
+export default ContactInfoForm;
