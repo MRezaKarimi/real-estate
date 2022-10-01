e0c2fa9403f2d7e9b9e343d749dd1ea52ca200d0
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-10-01 12:48:59 +0330

    add propertyDetailsForm component

---
+++ src/components/propertyDetailsForm.jsx
@@ -0,0 +1,30 @@
+import { faDollar, faRuler } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FormHeader from "./formHeader";
+import InputField from "./inputField";
+import SelectorField from "./selectorField";
+
+const PropertyDetailsForm = ({ style }) => {
+  return (
+    <div
+      style={style}
+      className="transition-opacity ease-in duration-300 overflow-hidden"
+    >
+      <FormHeader title="Add Your Home Details" />
+      <div className="flex items-center mx-2 sm:mx-0">
+        <FontAwesomeIcon
+          icon={faDollar}
+          className="text-sky-900 text-xl mr-2"
+        />
+        <InputField label="" placeholder="Price" name="price" />
+        <FontAwesomeIcon icon={faRuler} className="text-sky-900 text-xl mx-2" />
+        <InputField label="" placeholder="Area" name="area" />
+      </div>
+      <SelectorField label="Bedrooms" />
+      <SelectorField label="Bathrooms" />
+      <InputField label="Address" placeholder="Your Home's Address" />
+    </div>
+  );
+};
+
+export default PropertyDetailsForm;
