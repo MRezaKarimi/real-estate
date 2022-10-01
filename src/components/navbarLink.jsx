a41a138685ebfa29a1d76043607e62c5b36b5784
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-10-01 16:23:49 +0330

    add className param

--- src/components/navbarLink.jsx
+++ src/components/navbarLink.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavBarLink = ({ label, to }) => {
+const NavBarLink = ({ label, to, className = "" }) => {
   return (
-    <Link className="text-sky-700 text-md font-semibold px-2" to={to}>
+    <Link
+      className={`text-sky-700 text-md font-semibold px-2 ${className}`}
+      to={to}
+    >
       {label}
     </Link>
   );
