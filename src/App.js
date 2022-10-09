3c2bfa237d7941064ae4c2538992bdf987f9fe6b
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-10-09 10:43:24 +0330

    add property detail route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./App.css";
 import SellHome from "./components/sellHome";
 import Login from "./components/login";
 import SignUp from "./components/signUp";
+import PropertyDetails from "./components/propertyDetails";
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           <Routes>
             <Route path="/buy" element={<BuyHome />}></Route>
             <Route path="/sell" element={<SellHome />}></Route>
+            <Route path="/property/:id" element={<PropertyDetails />}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/sign-up" element={<SignUp />}></Route>
             <Route path="/" element={<Main />}></Route>
