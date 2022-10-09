2978638a373d7835eb8342afdfd7e5e834f000b4
Mohammad R. Karimi <mr.karimi77@yahoo.com>
2022-10-09 10:27:24 +0330

    add icon prop. change text style.

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,7 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export const OutlinedButton = ({
   text,
+  icon,
   onClick,
   type = "button",
   disabled = false,
@@ -9,13 +11,14 @@ export const OutlinedButton = ({
   return (
     <button
       type={type}
-      className={`h-10 flex items-center justify-center border-2 rounded-lg px-4 py-1 my-2 font-semibold ${
+      className={`h-10 flex items-center justify-center border-2 rounded-lg px-4 py-1 my-2 font-semibold text-sm ${
         disabled
           ? "cursor-not-allowed border-gray-400 text-gray-400"
           : "cursor-pointer border-sky-700 text-sky-700"
       }`}
       onClick={disabled ? null : onClick}
     >
+      {icon && <FontAwesomeIcon icon={icon} className="mr-2" />}
       {text}
     </button>
   );
@@ -23,6 +26,7 @@ export const OutlinedButton = ({
 
 export const FilledButton = ({
   text,
+  icon,
   onClick,
   type = "button",
   disabled = false,
@@ -30,13 +34,14 @@ export const FilledButton = ({
   return (
     <button
       type={type}
-      className={`h-10 flex items-center justify-center rounded-lg px-4 py-1 my-2 text-white font-semibold ${
+      className={`h-10 flex items-center justify-center rounded-lg px-4 py-1 my-2 text-white font-semibold text-sm ${
         disabled
           ? "cursor-not-allowed bg-gray-400"
           : "cursor-pointer bg-sky-700"
       }`}
       onClick={disabled ? null : onClick}
     >
+      {icon && <FontAwesomeIcon icon={icon} className="mr-2" />}
       {text}
     </button>
   );
