d619c4b0c5c62702e8c6e0679f83d563cf884f84
mark <mr.karimi1377@gmail.com>
2022-11-11 12:33:26 +0330

    show error message when property list is empty

--- src/components/propertyList.jsx
+++ src/components/propertyList.jsx
@@ -31,11 +31,20 @@ const PropertyList = () => {
         <div className="flex justify-center items-center h-[calc(100vh-4rem)]">
           <Ellipsis color="#0369a1" />
         </div>
-      ) : (
+      ) : propertyList && propertyList.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-8 gap-x-4 px-4">
           {propertyList.map((property, i) => (
             <PropertyItem key={i} property={property} />
           ))}
+        </div>
+      ) : (
+        <div className="h-[calc(100vh-4rem)] flex flex-col justify-center items text-center text-sky-900">
+          <span className="font-bold text-5xl my-7">Oops!</span>
+          <span className="font-semibold text-lg">
+            No property was found!
+            <br />
+            Try searching in another city
+          </span>
         </div>
       )}
 
